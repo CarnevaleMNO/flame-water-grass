6508237cc9680c15e0c7dc8f7f9048abc19cbdf0
Matthew Carnevale <carnevalemno@gmail.com>
2021-10-06 10:44:02 +0900

    Add HTML

--- app.js
+++ app.js
@@ -14,37 +14,49 @@ const computerPlay = () => {
     return result
 };
 
+const result = document.querySelector('#results')
+
 const playGame = (playerSelection, cpuSelection) => {
     playerSelection = playerSelection.toLowerCase()
 
     if (playerSelection === 'rock' && cpuSelection === 'scissors') {
+        result.innerHTML = 'You win!'
         console.log('You win!')
         return playerWins += 1;
     } else if (playerSelection === 'rock' && cpuSelection === 'paper') {
+        result.innerHTML = 'You lose!'
         console.log('You lose!')
         return cpuWins += 1;
     } else if (playerSelection === 'rock' && cpuSelection === 'rock') {
+        result.innerHTML = 'It is a tie!'
         console.log('It is a tie!')
         return;
     } else if (playerSelection === 'paper' && cpuSelection === 'scissors') {
+        result.innerHTML = 'You lose!'
         console.log('You lose!')
         return cpuWins += 1;
     } else if (playerSelection === 'paper' && cpuSelection === 'paper') {
+        result.innerHTML = 'It is a tie!'
         console.log('It is a tie!')
         return;
     } else if (playerSelection === 'paper' && cpuSelection === 'rock') {
+        result.innerHTML = 'You win!'
         console.log('You win!')
         return playerWins += 1;
     } else if (playerSelection === 'scissors' && cpuSelection === 'scissors') {
+        result.innerHTML = 'It is a tie!'
         console.log('It is a tie!')
         return;
     } else if (playerSelection === 'scissors' && cpuSelection === 'paper') {
+        result.innerHTML = 'You win!'
         console.log('You win!')
         return playerWins += 1;
     } else if (playerSelection === 'scissors' && cpuSelection === 'rock') {
+        result.innerHTML = 'You lose!'
         console.log('You lose!')
         return cpuWins += 1;
     } else {
+        result.innerHTML = 'Error. Please choose from paper, rock, or scissors.'
         return 'Error. Please choose from paper, rock or scissors.'
     }
 };
@@ -60,6 +72,8 @@ form.addEventListener('submit', function(eventObject){
 
 let playerWins = 0;
 let cpuWins = 0;
+const p1 = document.querySelector('#p1');
+const c1 = document.querySelector('#c1');
 console.log(`Player Score: ${playerWins}`);
 console.log(`Computer Score: ${cpuWins}`);
 
@@ -67,6 +81,8 @@ btn.addEventListener('click', function() {
     let playerSelection = playerInput.value
     let cpuSelection = computerPlay();
     playGame(playerSelection, cpuSelection)
+    p1.innerHTML = `Player Score: ${playerWins}`
+    c1.innerHTML = `Computer Score: ${cpuWins}`
     console.log(`Player Score: ${playerWins}`);
     console.log(`Computer Score: ${cpuWins}`);
 });
